d9100073bbebfe628f949cb1009dae795f74d22e
Camilo Campillo <147271067+CamiloCampillo@users.noreply.github.com>
2025-01-07 14:47:50 -0500

    Add files via upload

---
+++ about.R
@@ -0,0 +1,18 @@
+---
+  title: "Camilo Campillo"
+about:
+  template: jolla
+image: profile.jpeg
+links:
+  - icon: linkedin
+text: linkedin
+href: https://www.linkedin.com/in/camilo-campillo-mart%C3%ADnez/
+  - icon: github
+text: Github
+href: https://github.com/CamiloCampillo
+- icon: envelope
+text: Correo
+href: mailto:camilocampillo07@gmail.com
+---
+  
+  Hola, soy un joven curioso al que le encanta aprender de todo un poco, desde ciencia de datos hasta proyectos súper creativos. Siempre estoy buscando nuevos retos y formas de dejar mi huella (o al menos intentarlo 🙃).
